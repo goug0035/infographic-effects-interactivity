4beb06db1fc8651a49391e9786c95875411c1d5d
goug0035 <marygough@Marys-MacBook-Pro.local>
2016-04-23 20:27:09 -0400

    spacing fixes

--- js/main.js
+++ js/main.js
@@ -8,27 +8,27 @@ var $body = $('body');
 
 $coke.waypoint(function () {
   $coke.addClass('shake');
-}, { offset: '30%' });
+}, {offset: '30%'});
 
 $pino.waypoint(function () {
   $pino.addClass('shake');
-}, { offset: '30%' });
+}, {offset: '30%'});
 
 $wine.waypoint(function () {
   $wine.addClass('shake');
-}, { offset: '30%' });
+}, {offset: '30%'});
 
 $liq.waypoint(function () {
   $liq.addClass('shake');
-}, { offset: '30%' });
+}, {offset: '30%'});
 
 $pint.waypoint(function () {
   $pint.addClass('shake');
-}, { offset: '30%' });
+}, {offset: '30%'});
 
 $marga.waypoint(function () {
   $marga.addClass('shake');
-}, { offset: '30%' });
+}, {offset: '30%'});
 
 $body.on('click', '.btn', function () {
   $('.muffin').toggleClass('move');
