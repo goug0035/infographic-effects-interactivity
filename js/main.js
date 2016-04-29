2a4e85789446805af1dbc51524e183794b980de8
goug0035 <marygough@Marys-MacBook-Pro.local>
2016-04-29 19:28:26 -0400

    done but need to fix line 83

--- js/main.js
+++ js/main.js
@@ -4,33 +4,52 @@ var $wine = $('.wine');
 var $liq = $('.liq');
 var $pint = $('.pint');
 var $marga = $('.marga');
-var $body = $('body');
 var $drinks = $('.drinks');
 
 $drinks.waypoint(function () {
   $coke.addClass('see');
-}, {offset: '90%'});
-
-$drinks.waypoint(function () {
-  $pino.addClass('see');
-}, {offset: '80%'});
-
-$drinks.waypoint(function () {
-  $wine.addClass('see');
-}, {offset: '70%'});
-
-$drinks.waypoint(function () {
-  $liq.addClass('see');
-}, {offset: '60%'});
-
-$drinks.waypoint(function () {
-  $pint.addClass('see');
 }, {offset: '50%'});
 
 $drinks.waypoint(function () {
-  $marga.addClass('see');
+  $pino.addClass('see');
 }, {offset: '40%'});
 
-$body.on('click', function () {
-  $coke.removeClass('muffin');
+$drinks.waypoint(function () {
+  $wine.addClass('see');
+}, {offset: '30%'});
+
+$drinks.waypoint(function () {
+  $liq.addClass('see');
+}, {offset: '20%'});
+
+$drinks.waypoint(function () {
+  $pint.addClass('see');
+}, {offset: '10%'});
+
+$drinks.waypoint(function () {
+  $marga.addClass('see');
+}, {offset: '0%'});
+
+$drinks.on('click', '.coke', function () {
+  $('.muffin').removeClass('move');
+});
+
+$drinks.on('click', '.pino', function () {
+  $('.dounut').removeClass('move');
+});
+
+$drinks.on('click', '.wine', function () {
+  $('.cake').removeClass('move');
+});
+
+$drinks.on('click', '.liq', function () {
+  $('.sausage').removeClass('move');
+});
+
+$drinks.on('click', '.pint', function () {
+  $('.pizza').removeClass('move');
+});
+
+$drinks.on('click', '.marga', function () {
+  $('.burger').removeClass('move');
 });
